9d37b693c32f889a8225c7ab56eaa578cefcbeb4
Becky828 <38361070+Becky828@users.noreply.github.com>
2023-09-21 12:27:04 -0700

    Correctly updated relevant CMake and c++ files to conform with CMakeHw1Q4.

---
+++ vs_17.7.4/CMakeHw1Q4/CMakeHw1Q4.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeHw1Q4.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeHw1Q4.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
