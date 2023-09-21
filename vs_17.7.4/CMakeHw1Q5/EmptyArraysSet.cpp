6bec5fc385369f94254e81c1a526ad7d7b31bbf1
Becky828 <38361070+Becky828@users.noreply.github.com>
2023-09-21 13:16:58 -0700

    Now implementing vectors instead of arrays.

--- vs_17.7.4/CMakeHw1Q5/EmptyArraysSet.cpp
+++ vs_17.7.4/CMakeHw1Q5/EmptyArraysSet.cpp
@@ -1,8 +1,14 @@
+#include <vector>
 struct C
 {
-	int c1[20] = {}; 
+	std::vector<int> c1 = {};
+	std::vector<int> c2 = {};
+	std::vector<int> c3 = {};
+	std::vector<int> c4 = {};
+	std::vector<int> c5 = {};
+	/*int c1[20] = {};
 	int c2[20] = {};
 	int c3[40] = {};
-	int c4[50] = {};
+	int c4[50] = {};*/
 	//int c5[10] = {};
 };
---
+++ vs_17.7.4/CMakeHw1Q5/merge.cpp
@@ -0,0 +1,4 @@
+//int[] merge()
+//{
+//	return 
+//}
