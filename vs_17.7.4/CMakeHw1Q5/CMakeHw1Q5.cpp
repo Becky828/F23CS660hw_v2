d09ead0a3b4ddca3fd23b9c3ab38667b83a0b8d1
Becky828 <38361070+Becky828@users.noreply.github.com>
2023-09-21 12:07:53 -0700

    Correctly updated relevant CMake and c++ files to pertain to CMakeHw1Q5.

---
+++ vs_17.7.4/CMakeHw1Q5/CMakeHw1Q5.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeHw1Q5.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeHw1Q5.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
