f32ff3fe7a17b737176afc71e09313995bc5874d
Becky828 <38361070+Becky828@users.noreply.github.com>
2023-09-21 13:50:28 -0700

    Attempted to implement merge as shown in psuedocode on page 51 of Dasgupta textbook.

--- vs_17.7.4/CMakeHw1Q5/merge.cpp
+++ vs_17.7.4/CMakeHw1Q5/merge.cpp
@@ -1,4 +1,28 @@
-//int[] merge()
-//{
-//	return 
-//}
+#include <vector>
+
+std::vector<int> merge(std::vector<int> arrayOne, std::vector<int> arrayTwo)
+{
+	std::vector<int> result;
+	if (arrayOne[arrayOne.size() - 1 == 0])
+		return arrayTwo;
+	if (arrayTwo[arrayTwo.size() - 1 == 0])
+		return arrayOne;
+	if (arrayOne[1] <= arrayTwo[1])
+	{
+		result.reserve(1+arrayOne.size());
+		arrayOne.erase(arrayOne.begin());
+		std::vector<int>  recursiveArray = merge(arrayOne, arrayTwo);
+
+		result.insert(result.end(), arrayOne[0]);
+		result.insert(result.end(), recursiveArray.begin(), recursiveArray.end());
+	}
+	else
+	{
+		result.reserve(1 + arrayTwo.size());
+		arrayTwo.erase(arrayTwo.begin());
+		std::vector<int>  recursiveArray = merge(arrayOne, arrayTwo);
+
+		result.insert(result.end(), arrayTwo[0]);
+		result.insert(result.end(), recursiveArray.begin(), recursiveArray.end());
+	}	
+};
