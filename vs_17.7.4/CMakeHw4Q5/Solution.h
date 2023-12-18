73e07f3676795f83b92d3744a2d8190cea15d059
Becky828 <38361070+Becky828@users.noreply.github.com>
2023-12-17 20:42:01 -0800

    I have reached a best effort solution returns the correct response by finding the ccnum sum and using it to modify a copy of graph G and a copy of stones vector.

--- vs_17.7.4/CMakeHw4Q5/Solution.h
+++ vs_17.7.4/CMakeHw4Q5/Solution.h
@@ -5,31 +5,40 @@
 
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 
 class Solution {
 public:
+    Solution() { n = 0; cc = 0; };
     /*const int neededSize;*/
     void explore(vector<vector<int>> G, vector<int> s, int vertex);
     void previsit(int vertex);
-    void postvisit(int vertex); 
+   // void postvisit(int vertex); 
    // vector<bool> visited;
     //vector< vector<int> > traveledPath;
     
-    /*int cc;
-    vector<int> ccnum;*/
+    int cc;
+    vector<int> ccnum;
     vector<bool> visited;
-    vector<int> pre;
-    vector<int> post;
+
+   vector<vector<int>> stonesCopy;
+    vector<vector<int>> removedStones;
+
+    vector<vector<int>> GCopy;
+   /* vector<int> pre;
+    vector<int> post;*/
     int n;
-    int clock;
+    //int clock;
 
     void buildGraph(vector<vector<int>>& stones);
-    void buildReversedGraph(vector<vector<int>> G);
+  //  void buildReversedGraph(vector<vector<int>> G);
     int removeStones(vector<vector<int>>& stones);
+    void dfs();
+    void init(std::vector<std::vector<int>>& stones);
     vector<vector<int>> G{ {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
-    vector<vector<int>> GReversed{ {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
+   // vector<vector<int>> GReversed{ {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
 
 
 };
